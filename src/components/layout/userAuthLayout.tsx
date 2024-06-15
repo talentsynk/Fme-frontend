4a48311e9cf780d060fe63e27627d2d4d785b3ad
Jude Tochy <tochyokoye@gmail.com>
2024-06-15 20:22:28 +0100

    added right icons and added text animation

--- src/components/layout/userAuthLayout.tsx
+++ src/components/layout/userAuthLayout.tsx
@@ -3,8 +3,24 @@ import { CoderinaLogo } from "@/app/recovery/style";
 import { FGLogo, FullLogo } from "../icons/sidebar";
 import { PoweredByStyles, UserAuthLayoutStyles } from "./style";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+
+const Words = [
+  "The home to skilled artisans",
+  "We connect You to skilled artisans",
+];
 
 export const UserAuthLayout = ({ children }: { children: React.ReactNode }) => {
+  const [words, setWords] = useState(Words);
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prevIndex) => (prevIndex + 1) % words.length);
+    }, 3000);
+    return () => clearInterval(interval);
+  }, [words.length]);
   return (
     <UserAuthLayoutStyles>
       <div className="one desktop">
@@ -12,7 +28,21 @@ export const UserAuthLayout = ({ children }: { children: React.ReactNode }) => {
           <FullLogo textcolor="#fff" />
         </div>
         <div className="text">
-          <h3>The home to skilled artisans</h3>
+          <h3>
+            {" "}
+            <AnimatePresence mode="wait">
+              <motion.span
+                key={index}
+                className="f"
+                initial={{ opacity: 0, y: 25 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: 100 }}
+                transition={{ duration: 0.5 }}
+              >
+                {words[index]}
+              </motion.span>
+            </AnimatePresence>
+          </h3>
           <p>
             Our comprehensive design system offers you an unparalleled range of
             components, sparking creativity and boosting efficiency.
