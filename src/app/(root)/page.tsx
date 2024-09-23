749c217841b23060468ac1cc09427754d12c2008
Samuel <emorinkensam@gmail.com>
2024-09-23 04:32:00 -0500

    changed texts on the landing page

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -53,9 +53,9 @@ export default function Home() {
         </div>
         <div className=" flex flex-col md:flex-row space-y-2 p-8 justify-between py-16">
           <div className="text-white flex-1 space-y-6">
-            <h4 className=" text-[18px] leading-[24px] font-medium text-center md:text-left">WHO WE ARE?</h4>
-            <h3 className=" text-[44px] font-medium text-center md:text-left leading-[48px]">We are all about</h3>
-            <p className=" text-[18px] text-center md:text-left py-2 md:py-0 leading-[24px] font-normal">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi labore exercitationem repellendus vero, minima quos quae maiores temporibus ad enim iste quibusdam veniam at voluptas numquam illo expedita maxime aliquid velit aliquam sint. Velit voluptas porro repudiandae quisquam cumque eius.</p>
+            <h4 className=" text-[18px] leading-[24px] font-medium text-center md:text-left">OUR OBJECTIVES</h4>
+            <h3 className=" text-[44px] font-medium text-center md:text-left leading-[48px]">CRAFTED WITH PROFESSIONALS AND EMPLOYERS IN MIND</h3>
+            <p className=" text-[18px] text-center md:text-left py-2 md:py-0 leading-[24px] font-normal">To truly drive transformation in Nigeria, it is essential to incorporate skills throughout the entire education sector. The council&apos;s efforts will be pivotal in achieving this ambitious goal.</p>
           </div>
           <div className=" flex-1 flex justify-end">
           <Image src="/images/fourth.png" width={512} height={468} alt="endorsement pictures" />
@@ -68,9 +68,9 @@ export default function Home() {
             <div onClick={handleEmployee} className={`cursor-pointer ${showEmployee?"bg-[#E7F6EC] text-[#00932E]":"text-[#98A2B3]"}  rounded-lg px-4 py-2.5 `}>For Employers</div>
             <div onClick={handleArtisans} className={`cursor-pointer ${!showEmployee?"bg-[#E7F6EC] text-[#00932E]":"text-[#98A2B3]"}  rounded-lg px-4 py-2.5 `}>For Artisans</div>
           </div>
-          <h5 className="animate-slideInLeft text-[18px] text-center md:text-left font-medium leading-[24px] text-[#101928]">OUR NEXT MILESTONE</h5>
-          <h2 className="animate-fadeIn  text-[36px] md:text-[44px] text-center md:text-left font-medium leading-[48px] text-[#101928]">We Are National Skills Information Center</h2>
-          <p className="animate-bounce text-[18px] text-center md:text-left font-normal leading-[24px] text-[#101928]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati incidunt iusto ullam autem laborum harum, possimus nihil deleniti sint impedit debitis vero repellendus ab magnam ratione aperiam sequi. Minus sapiente fugiat, officia tempora accusantium numquam corporis dolorem maxime eaque recusandae!</p>
+          <h5 className="animate-slideInLeft text-[18px] text-center md:text-left font-medium leading-[24px] text-[#101928]">WHAT WE DO</h5>
+          <h2 className="animate-fadeIn  text-[36px] md:text-[44px] text-center md:text-left font-medium leading-[48px] text-[#101928]">We save time and effort in your next hiring journey</h2>
+          <p className="animate-bounce text-[18px] text-center md:text-left font-normal leading-[24px] text-[#101928]">When it comes to hiring the right artisans, it&apos;s essential to have a solution that streamlines the process. Our platform is equipped with a wide range of features designed to expedite candidate sourcing and screening.</p>
           <div className=" flex justify-center md:justify-start">
           <button className=" w-fit rounded-md py-4 px-6 border-2 border-[#00932E] bg-[#00932E] text-white font-bold transition duration-700 ease-in-out  mt-12 hover:text-[#00932E] hover:bg-white  hover:border-[#00932E] hover:border-[1px] hover:border-solid">Get started today!</button>
           </div>
@@ -96,7 +96,7 @@ export default function Home() {
        </div>
        <div className=" space-y-8 py-16">
         <h3 className="animate-slideInLeft text-center text-[#101928] text-[44px] leading-[48px] font-bold">The essential for <span className=" text-[#00932E]">skilled professionals</span></h3>
-        <p className="animate-bounce text-center md:w-1/2 mx-2 md:mx-auto  text-[18px] leading-[24px] font-normal text-black">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum exercitationem ullam expedita aut dolore impedit deleniti cumque nemo sit tempora.</p>
+        <p className="animate-bounce text-center md:w-1/2 mx-2 md:mx-auto  text-[18px] leading-[24px] font-normal text-black">Whether you&apos;re a carpenter, plumber, electrician, tailor, or any other type of artisan, National Skills Information Center is your go-to marketplace to find work opportunities and grow your business.</p>
         <div className="carousel-item flex-none   carousel-item  flex flex-col space-y-4 px-2 carousel  md:flex-row gap-4 items-center md:items-start md:overflow-x-auto scrollbar-hide w-full">
           <div className=" my-2  relative h-[350px]">
 
@@ -147,11 +147,11 @@ export default function Home() {
        <div className="artisan bg-[#00932E] animate-fadeIn  space-y-16  md:space-y-0 flex gap-8 flex-col md:flex-row justify-between py-32 p-16">
         <div className=" flex flex-col md:flex-row space-y-4 md:space-y-0 gap-8">
         <div className=" space-y-4 ">
-          <h2 className=" text-white text-[72px] leading-[56px] font-medium">3132+</h2>
+          <h2 className=" text-white text-[72px] leading-[56px] font-medium">17+</h2>
           <p className=" text-[18px] font-medium leading-[24px] text-white">TOTAL TRAINED ARTISANS</p>
         </div>
         <div className=" space-y-4 ">
-          <h2 className=" text-white text-[72px] leading-[56px] font-medium">10000</h2>
+          <h2 className=" text-white text-[72px] leading-[56px] font-medium">1</h2>
           <div className=" flex items-center gap-1">
             <GradCap />
           <p className="text-[18px] font-medium leading-[24px] text-white">CERTIFIED ARTISANS</p>
@@ -165,7 +165,7 @@ export default function Home() {
        </div>
        <div className=" flex p-16 gap-4 flex-col md:flex-row bg-white">
         <div className=" flex flex-col gap-4 flex-1 md:pt-12 md:pb-28">
-          <h2 className="animate-fadeIn  text-[36px] md:text-[44px] text-center md:text-left leading-[32px] md:leading-[48px] font-medium text-[#101928] ">We are National Skills Information Center</h2>
+          <h2 className="animate-fadeIn  text-[36px] md:text-[44px] text-center md:text-left leading-[32px] md:leading-[48px] font-medium text-[#101928] ">Try out the future of the day!</h2>
           <p className="animate-slideInLeft text-center md:text-left text-[#101928] font-normal text-[16px] md:text-[18px] leading-[24px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam esse optio error ducimus repudiandae! Quae placeat expedita rerum ducimus libero rem ipsam, enim perferendis ipsa aperiam, ipsum assumenda. Sequi saepe illo eveniet, minima veritatis minus iste repudiandae cumque perferendis eos nesciunt, facere, ratione dicta cum beatae. Eligendi a laborum magni.</p>
           <button className="w-fit rounded-md py-4 px-6 border-2 border-[#00932E] bg-[#00932E] text-white font-bold transition duration-700 ease-in-out  mx-auto md:mx-0  mt-24 hover:text-[#00932E] hover:bg-white  hover:border-[#00932E] hover:border-[1px] hover:border-solid">View profile</button>
         </div>
